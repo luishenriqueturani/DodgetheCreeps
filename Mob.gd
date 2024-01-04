01d320e433cc3d2aea99e4948aa4d8432facbf58
Luís Henrique <79663087+luishenriqueturani@users.noreply.github.com>
2024-01-04 11:00:07 -0300

    Foi feito sem o git, depois foi dado o commit de tudo o que foi feito

---
+++ Mob.gd
@@ -0,0 +1,18 @@
+extends RigidBody2D
+
+export var min_speed = 150
+export var max_speed = 250
+var mob_types = ["walk", "swim", "fly"]
+
+
+func _ready():
+	$AnimatedSprite.animation = mob_types[randi() % mob_types.size()]
+	pass # Replace with function body.
+
+
+
+
+
+func _on_Visibility_screen_exited():
+	queue_free()
+	pass # Replace with function body.
